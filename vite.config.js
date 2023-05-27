31cd086c277da653cbfe6f1474cb3d7b749631bb
Aiden Bai <aiden.bai05@gmail.com>
2023-05-26 17:06:20 -0700

    pad

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import legacy from '@vitejs/plugin-legacy';
-import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [react(), legacy(), VitePWA()],
+  plugins: [react()],
 });
