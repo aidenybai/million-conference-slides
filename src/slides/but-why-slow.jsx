fd2ef71f170cb2166395a727ef5c71179082f295
Aiden Bai <aiden.bai05@gmail.com>
2023-06-14 00:34:46 -0700

    reorder

--- src/slides/but-why-slow.jsx
+++ src/slides/but-why-slow.jsx
@@ -3,26 +3,53 @@ import { bodyClassNameAtom } from '../layout';
 import { useEffect } from 'react';
 import { motion } from 'framer-motion';
 
-export default () => {
-  const [_, setClassName] = useAtom(bodyClassNameAtom);
+export default [
+  () => {
+    const [_, setClassName] = useAtom(bodyClassNameAtom);
 
-  useEffect(() => {
-    setClassName(() => 'bg-white');
-  }, []);
+    useEffect(() => {
+      setClassName(() => 'bg-white');
+    }, []);
 
-  return (
-    <motion.div
-      initial={{ scale: 0.9, opacity: 0 }}
-      animate={{ scale: 1, opacity: 1 }}
-      transition={{
-        ease: 'easeInAndOut',
-        type: 'spring',
-        stiffness: 260,
-        damping: 20,
-      }}
-      className="text-7xl text-center font-bold flex flex-col justify-center items-center gap-5 leading-normal"
-    >
-      <p>But why is it slow?</p>
-    </motion.div>
-  );
-};
+    return (
+      <motion.div
+        initial={{ scale: 0.9, opacity: 0 }}
+        animate={{ scale: 1, opacity: 1 }}
+        transition={{
+          ease: 'easeInAndOut',
+          type: 'spring',
+          stiffness: 260,
+          damping: 20,
+        }}
+        className="text-7xl text-center font-bold flex flex-col justify-center items-center gap-5 leading-normal"
+      >
+        <p>But why is it slow?</p>
+      </motion.div>
+    );
+  },
+  () => {
+    const [_, setClassName] = useAtom(bodyClassNameAtom);
+
+    useEffect(() => {
+      setClassName(() => 'bg-white');
+    }, []);
+
+    return (
+      <motion.div
+        initial={{ scale: 0.9, opacity: 0 }}
+        animate={{ scale: 1, opacity: 1 }}
+        transition={{
+          ease: 'easeInAndOut',
+          type: 'spring',
+          stiffness: 260,
+          damping: 20,
+        }}
+        className="text-7xl text-center font-bold flex flex-col justify-center items-center gap-5 leading-normal"
+      >
+        <p>
+          But <u>can it</u> be slow?
+        </p>
+      </motion.div>
+    );
+  },
+];
